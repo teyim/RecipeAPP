74dba67962994acb3b54104bb3e93bdb4320996b
Teyim <teyimasobo@gmail.com>
2020-10-11 17:14:05 +0100

    Updates

---
+++ precache-manifest.dcce7282d35c0b3ab2ab05e76542f3bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb83085be6e9b7fa7854981faab94687",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32823f0d79a005c1b16e",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7e1c03e1605b3270b59c",
+    "url": "./static/css/main.153613a5.chunk.css"
+  },
+  {
+    "revision": "32823f0d79a005c1b16e",
+    "url": "./static/js/2.2df1b760.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2df1b760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1c03e1605b3270b59c",
+    "url": "./static/js/main.47e999b9.chunk.js"
+  },
+  {
+    "revision": "4eac92124f02cde28de5",
+    "url": "./static/js/runtime-main.23a3178f.js"
+  },
+  {
+    "revision": "2b95350e1282f28f123cab77b48a535c",
+    "url": "./static/media/1161745-312x231.2b95350e.jpg"
+  },
+  {
+    "revision": "0527b4b9d8ff94d672d6e78b753fd61f",
+    "url": "./static/media/icons8_delete.0527b4b9.ico"
+  },
+  {
+    "revision": "12bd97a8515fcd1f3d03059f44839575",
+    "url": "./static/media/icons8_pot.12bd97a8.ico"
+  },
+  {
+    "revision": "5f844aae80a2472cc74689b364319f96",
+    "url": "./static/media/icons8_soup_plate_32.5f844aae.png"
+  },
+  {
+    "revision": "a3803f267e0844ada17550537aff736b",
+    "url": "./static/media/icons8_stopwatch.a3803f26.ico"
+  },
+  {
+    "revision": "04b5130bf2bdf2b391536b8e61efe5b2",
+    "url": "./static/media/kisspng-angus-cattle-kebab-hamburger-take-out-pizza-beef-burger-5ad848f1a59fd3.9041058515241238896784.04b5130b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7BRecipeAPP%7D/precache-manifest.45bf98203f1ae559a6aedbdd461d9401.js"
+  "./precache-manifest.dcce7282d35c0b3ab2ab05e76542f3bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7BRecipeAPP%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
