2d7534e71f5275007eb0ce7357d096987d02e1d4
Teyim <teyimasobo@gmail.com>
2020-10-11 17:00:48 +0100

    Updates

---
+++ precache-manifest.45bf98203f1ae559a6aedbdd461d9401.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267c7ea18af2bbf560a59757f41e1d12",
+    "url": "/%7BRecipeAPP%7D/index.html"
+  },
+  {
+    "revision": "32823f0d79a005c1b16e",
+    "url": "/%7BRecipeAPP%7D/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a251e217164886814a79",
+    "url": "/%7BRecipeAPP%7D/static/css/main.c4d5ff7a.chunk.css"
+  },
+  {
+    "revision": "32823f0d79a005c1b16e",
+    "url": "/%7BRecipeAPP%7D/static/js/2.2df1b760.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%7BRecipeAPP%7D/static/js/2.2df1b760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a251e217164886814a79",
+    "url": "/%7BRecipeAPP%7D/static/js/main.ffd59e1e.chunk.js"
+  },
+  {
+    "revision": "c6dfc7c2cc80caf9da61",
+    "url": "/%7BRecipeAPP%7D/static/js/runtime-main.0822e637.js"
+  },
+  {
+    "revision": "2b95350e1282f28f123cab77b48a535c",
+    "url": "/%7BRecipeAPP%7D/static/media/1161745-312x231.2b95350e.jpg"
+  },
+  {
+    "revision": "0527b4b9d8ff94d672d6e78b753fd61f",
+    "url": "/%7BRecipeAPP%7D/static/media/icons8_delete.0527b4b9.ico"
+  },
+  {
+    "revision": "12bd97a8515fcd1f3d03059f44839575",
+    "url": "/%7BRecipeAPP%7D/static/media/icons8_pot.12bd97a8.ico"
+  },
+  {
+    "revision": "5f844aae80a2472cc74689b364319f96",
+    "url": "/%7BRecipeAPP%7D/static/media/icons8_soup_plate_32.5f844aae.png"
+  },
+  {
+    "revision": "a3803f267e0844ada17550537aff736b",
+    "url": "/%7BRecipeAPP%7D/static/media/icons8_stopwatch.a3803f26.ico"
+  },
+  {
+    "revision": "04b5130bf2bdf2b391536b8e61efe5b2",
+    "url": "/%7BRecipeAPP%7D/static/media/kisspng-angus-cattle-kebab-hamburger-take-out-pizza-beef-burger-5ad848f1a59fd3.9041058515241238896784.04b5130b.png"
+  }
+]);
